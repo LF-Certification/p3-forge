4acb41d546865a3873a8252f2c9c5d21130c6044
Dessa Simpson <dsimpson@linuxfoundation.org>
2026-02-18 16:53:04 -0700

    fix(p3forge): Fix debian build

--- sandbox/vm/distro/debian/base/scripts/build.sh
+++ sandbox/vm/distro/debian/base/scripts/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bashp
 
+# Full kernel required for 9p mounts (debian cloud image defaults to stripped-down cloud kernel)
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y linux-image-amd64
+
 p3forge::base
