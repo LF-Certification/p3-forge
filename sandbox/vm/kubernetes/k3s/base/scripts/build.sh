2e3badb1b73af13b6bbe83078eeca7bfc96758aa
Dessa Simpson <dsimpson@linuxfoundation.org>
2026-02-26 11:02:27 -0700

    fix(k3s): Create /root/.kube

--- sandbox/vm/kubernetes/k3s/base/scripts/build.sh
+++ sandbox/vm/kubernetes/k3s/base/scripts/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="v${K8S_VERSION:?must be set}" sh -
 
+mkdir -p ~/.kube
 sudo -u tux mkdir -p ~tux/.kube
 for homedir in ~ ~tux; do file::copy /etc/rancher/k3s/k3s.yaml "$homedir"/.kube/config; done
 k8s::configure_shell
