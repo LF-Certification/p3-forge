600f35cc0ecca1a143a2db6ca7ed9fe59e9e2df5
Dessa Simpson <dsimpson@linuxfoundation.org>
2026-03-05 13:59:38 -0700

    fix(sandbox): Key debian kernel install to arch

--- sandbox/vm/distro/debian/base/scripts/build.sh
+++ sandbox/vm/distro/debian/base/scripts/build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bashp
 
 # Full kernel required for 9p mounts (debian cloud image defaults to stripped-down cloud kernel)
+arch="$(system::get_arch)"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y linux-image-amd64
-apt-get purge -y linux-image-cloud-amd64
+apt-get install -y "linux-image-$arch"
+apt-get purge -y "linux-image-cloud-$arch"
 dpkg --purge --force-depends $(dpkg -l | awk '/linux-image.*cloud/ {print $2}')
 
 p3forge::base
