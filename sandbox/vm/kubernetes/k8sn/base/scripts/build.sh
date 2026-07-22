d867f3cee09a7df378ee9b5c0d4e1bcdb0df5ffa
Jean-Luc Davern <jdavern@linuxfoundation.org>
2026-07-22 17:53:27 -0500

    chore(sandbox/vm): pinned nerdctl version in build script
    - Replaced the GitHub API lookup with a fixed nerdctl release version.
    - Tightened the download command to fail on HTTP errors.
    
    Signed-off-by: Jean-Luc Davern <jdavern@linuxfoundation.org>

--- sandbox/vm/kubernetes/k8sn/base/scripts/build.sh
+++ sandbox/vm/kubernetes/k8sn/base/scripts/build.sh
@@ -3,5 +3,5 @@
 kubeVersion="$K8S_VERSION" kubeadm::prepare
 
 # Install nerdctl to allow for self-contained multi-node
-NERDCTL_VERSION=$(curl -s https://api.github.com/repos/containerd/nerdctl/releases/latest | yq -r '.tag_name')
-curl -sL "https://github.com/containerd/nerdctl/releases/download/${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION#v}-linux-$(system::get_arch).tar.gz" | tar -xzC /usr/local/bin/ nerdctl
+NERDCTL_VERSION=v2.3.5
+curl -fsSL "https://github.com/containerd/nerdctl/releases/download/${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION#v}-linux-$(system::get_arch).tar.gz" | tar -xzC /usr/local/bin/ nerdctl
